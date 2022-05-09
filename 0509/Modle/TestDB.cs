0015f13b1cddf102b894573ed1a514c2c53786d9
b9704 <b9704@LAPTOP-NR9TFARA>
2022-05-09 10:43:04 +0800

    加入專案檔案。

---
+++ 0509/Modle/TestDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace _0509.Modle
+{
+    public class TestDB : DbContext
+    {
+        public TestDB(DbContextOptions<TestDB> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<TodoItem> TodoItem { get; set; }
+    }
+}
---
+++ 0509/Modle/TodoItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _0509.Modle
+{
+    public class TodoItem
+    {
+        /// <summary>
+        /// 識別id
+        /// </summary>
+        public long id { get; set; }
+
+        /// <summary>
+        /// 代辦事項名稱
+        /// </summary>
+        public string name { get; set; }
+
+        /// <summary>
+        /// 是否完成
+        /// </summary>
+        public bool isComplete { get; set; }
+    }
+}
